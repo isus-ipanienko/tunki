b2994ee8287ad2a493fb197167e2a4dd75fc5cf9
isus-ipanienko <preliga@proton.me>
2024-05-20 22:47:10 +0200

    Finish transition to zig

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const Cartridge = u8[0x8000];
+const Cartridge = [0x7FFF]u8;
 
 const Op = enum(u8) {
     RET = 0x00,
@@ -43,221 +43,228 @@ const Registers = struct {
 
 const Cpu = struct {
     reg: Registers,
-    mem: u8[0xFFFF],
+    mem: [0xFFFF]u8,
 
-    fn read_u8(pos: u16) u8 {
+    fn read_u8(self: Cpu, pos: u16) u8 {
         // TODO: add logging
-        return .mem[pos];
+        return self.mem[pos];
     }
-    fn write_u8(pos: u16, data: u8) void {
+    fn write_u8(self: *Cpu, pos: u16, data: u8) void {
         // TODO: add logging
-        .mem[pos] = data;
+        self.mem[pos] = data;
     }
 
-    fn read_u16(pos: u16) u16 {
-        const hi: u16 = @as(u16, read_u8(pos + 1)) << 8;
-        const lo: u16 = read_u8(pos);
+    fn read_u16(self: Cpu, pos: u16) u16 {
+        const hi: u16 = @as(u16, self.read_u8(pos + 1)) << 8;
+        const lo: u16 = self.read_u8(pos);
         return hi | lo;
     }
-    fn write_u16(pos: u16, data: u16) void {
-        write_u8(pos, (data & 0xFF));
-        write_u8(pos + 1, (data >> 8));
+    fn write_u16(self: *Cpu, pos: u16, data: u16) void {
+        self.write_u8(pos, (data & 0xFF));
+        self.write_u8(pos + 1, (data >> 8));
     }
 
-    fn addr_immediate() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 1;
+    fn addr_immediate(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 1;
         return pc;
     }
-    fn addr_zero_page() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 1;
-        return read_u8(pc);
+    fn addr_zero_page(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 1;
+        return self.read_u8(pc);
     }
-    fn addr_zero_page_x() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 1;
-        const ret: u8 = read_u8(pc) + .reg.x;
+    fn addr_zero_page_x(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 1;
+        const ret: u8 = self.read_u8(pc) +% self.reg.x;
         return ret;
     }
-    fn addr_zero_page_y() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 1;
-        const ret: u8 = read_u8(pc) + .reg.y;
+    fn addr_zero_page_y(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 1;
+        const ret: u8 = self.read_u8(pc) +% self.reg.y;
         return ret;
     }
-    fn addr_absolute() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 2;
-        return read_u16(pc);
+    fn addr_absolute(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 2;
+        return self.read_u16(pc);
     }
-    fn addr_absolute_x() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 2;
-        return read_u16(pc) + .reg.x;
+    fn addr_absolute_x(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 2;
+        return self.read_u16(pc) +% self.reg.x;
     }
-    fn addr_absolute_y() u16 {
-        const pc: u16 = .reg.pc;
-        .reg.pc += 2;
-        return read_u16(pc) + .reg.y;
+    fn addr_absolute_y(self: *Cpu) u16 {
+        const pc: u16 = self.reg.pc;
+        self.reg.pc += 2;
+        return self.read_u16(pc) +% self.reg.y;
     }
-    fn addr_indirect_x() u16 {
-        const ptr: u8 = read_u8(.reg.pc) + .reg.x;
-        const hi: u16 = @as(u16, read_u8(ptr + 1)) << 8;
-        const lo: u16 = read_u8(ptr);
+    fn addr_indirect_x(self: Cpu) u16 {
+        const ptr: u8 = self.read_u8(self.reg.pc) +% self.reg.x;
+        const hi: u16 = @as(u16, self.read_u8(ptr +% 1)) << 8;
+        const lo: u16 = self.read_u8(ptr);
         return hi | lo;
     }
-    fn addr_indirect_y() u16 {
-        const ptr: u8 = read_u8(.reg.pc);
-        const hi: u16 = @as(u16, read_u8(ptr + 1)) << 8;
-        const lo: u16 = read_u8(ptr);
-        return (hi | lo) + @as(u16, .reg.y);
+    fn addr_indirect_y(self: Cpu) u16 {
+        const ptr: u8 = self.read_u8(self.reg.pc);
+        const hi: u16 = @as(u16, self.read_u8(ptr +% 1)) << 8;
+        const lo: u16 = self.read_u8(ptr);
+        return (hi | lo) +% @as(u16, self.reg.y);
     }
 
-    fn sta(addr: u16) void {
-        write_u8(addr, .reg.a);
+    fn sta(self: *Cpu, addr: u16) void {
+        self.write_u8(addr, self.reg.a);
     }
-    fn lda(addr: u16) void {
-        .reg.a = read_u8(addr);
-        .reg.z = .reg.a == 0;
-        .reg.n = .reg.a & (1 << 7);
+    fn lda(self: *Cpu, addr: u16) void {
+        self.reg.a = self.read_u8(addr);
+        self.reg.z = self.reg.a == 0;
+        self.reg.n = self.reg.a & (1 << 7) != 0;
     }
-    fn stx(addr: u16) void {
-        write_u8(addr, .reg.x);
+    fn stx(self: *Cpu, addr: u16) void {
+        self.write_u8(addr, self.reg.x);
     }
-    fn ldx(addr: u16) void {
-        .reg.x = read_u8(addr);
-        .reg.z = .reg.x == 0;
-        .reg.n = .reg.x & (1 << 7);
+    fn ldx(self: *Cpu, addr: u16) void {
+        self.reg.x = read_u8(addr);
+        self.reg.z = self.reg.x == 0;
+        self.reg.n = self.reg.x & (1 << 7) != 0;
     }
-    fn tax() void {
-        .reg.x = .reg.a;
-        .reg.z = .reg.x == 0;
-        .reg.n = .reg.x & (1 << 7);
+    fn tax(self: *Cpu) void {
+        self.reg.x = self.reg.a;
+        self.reg.z = self.reg.x == 0;
+        self.reg.n = self.reg.x & (1 << 7) != 0;
     }
-    fn inx() void {
-        .reg.x += 1;
-        .reg.z = .reg.x == 0;
-        .reg.n = .reg.x & (1 << 7);
+    fn inx(self: *Cpu) void {
+        self.reg.x += 1;
+        self.reg.z = self.reg.x == 0;
+        self.reg.n = self.reg.x & (1 << 7) != 0;
     }
 
-    pub fn exec() void {
+    pub fn exec(self: *Cpu) void {
         var opcode: Op = undefined;
         while (true) {
-            opcode = read_u8(.reg.pc);
-            .reg.pc += 1;
+            opcode = @enumFromInt(self.read_u8(self.reg.pc));
+            self.reg.pc += 1;
             switch (opcode) {
                 Op.STA_ZP => {
-                    sta(addr_zero_page());
+                    self.sta(self.addr_zero_page());
                 },
                 Op.STA_ZPX => {
-                    sta(addr_zero_page_x());
+                    self.sta(self.addr_zero_page_x());
                 },
                 Op.STA_A => {
-                    sta(addr_absolute());
+                    self.sta(self.addr_absolute());
                 },
                 Op.STA_AX => {
-                    sta(addr_absolute_x());
+                    self.sta(self.addr_absolute_x());
                 },
                 Op.STA_AY => {
-                    sta(addr_absolute_y());
+                    self.sta(self.addr_absolute_y());
                 },
                 Op.STA_IX => {
-                    sta(addr_indirect_x());
+                    self.sta(self.addr_indirect_x());
                 },
                 Op.STA_IY => {
-                    sta(addr_indirect_y());
+                    self.sta(self.addr_indirect_y());
                 },
                 Op.LDA_I => {
-                    lda(addr_immediate());
+                    self.lda(self.addr_immediate());
                 },
                 Op.LDA_ZP => {
-                    lda(addr_zero_page());
+                    self.lda(self.addr_zero_page());
                 },
                 Op.LDA_ZPX => {
-                    lda(addr_zero_page_x());
+                    self.lda(self.addr_zero_page_x());
                 },
                 Op.LDA_A => {
-                    lda(addr_absolute());
+                    self.lda(self.addr_absolute());
                 },
                 Op.LDA_AX => {
-                    lda(addr_absolute_x());
+                    self.lda(self.addr_absolute_x());
                 },
                 Op.LDA_AY => {
-                    lda(addr_absolute_y());
+                    self.lda(self.addr_absolute_y());
                 },
                 Op.LDA_IX => {
-                    lda(addr_indirect_x());
+                    self.lda(self.addr_indirect_x());
                 },
                 Op.LDA_IY => {
-                    lda(addr_indirect_y());
+                    self.lda(self.addr_indirect_y());
                 },
                 Op.STX_ZP => {
-                    stx(addr_zero_page());
+                    self.stx(self.addr_zero_page());
                 },
                 Op.STX_ZPY => {
-                    stx(addr_zero_page_y());
+                    self.stx(self.addr_zero_page_y());
                 },
                 Op.STX_A => {
-                    stx(addr_absolute());
+                    self.stx(self.addr_absolute());
                 },
                 Op.TAX => {
-                    tax();
+                    self.tax();
                 },
                 Op.INX => {
-                    inx();
+                    self.inx();
                 },
                 Op.RET => {
                     return;
                 },
-                else => {
-                    return;
-                },
+                // else => {
+                //     return;
+                // },
             }
         }
     }
 
-    pub fn reset() void {
-        .reg.sp = 0;
-        .reg.a = 0;
-        .reg.x = 0;
-        .reg.y = 0;
-        .reg.n = false;
-        .reg.v = false;
-        .reg.b = false;
-        .reg.d = false;
-        .reg.i = false;
-        .reg.z = false;
-        .reg.c = false;
-        .reg.pc = read_u16(0xFFFC);
+    pub fn reset(self: *Cpu) void {
+        self.reg.sp = 0;
+        self.reg.a = 0;
+        self.reg.x = 0;
+        self.reg.y = 0;
+        self.reg.n = false;
+        self.reg.v = false;
+        self.reg.b = false;
+        self.reg.d = false;
+        self.reg.i = false;
+        self.reg.z = false;
+        self.reg.c = false;
+        self.reg.pc = self.read_u16(0xFFFC);
     }
 
-    pub fn insert(cartridge: Cartridge) void {
-        std.mem.copy(&.mem[0x8000], &cartridge[0], cartridge.size());
-        reset();
+    pub fn insert(self: *Cpu, cartridge: Cartridge) void {
+        std.mem.copyForwards(u8, self.mem[0x8000..0xFFFF], &cartridge);
+        self.reset();
+    }
+
+    pub fn display(self: Cpu) void {
+        std.debug.print("\n", .{});
+        for (0.., self.mem) |i, m| {
+            if (m > 0) {
+                std.debug.print("0x{X}: 0x{X}\n", .{ i, m });
+            }
+        }
     }
 };
 
-test "test cpu" {
-    var cartridge: Cartridge = Cartridge;
-    cartridge[0x0001] = Op.LDA_I;
-    cartridge[0x0002] = 0x69;
-    cartridge[0x0003] = Op.TAX;
-    cartridge[0x0004] = Op.INX;
-    cartridge[0x0005] = Op.STA_ZPX;
-    cartridge[0x0006] = 0xF0;
-    cartridge[0x0007] = Op.RET;
+test "cpu" {
+    var cartridge: Cartridge = undefined;
+    @memset(&cartridge, 0x00);
+    cartridge[0x0000] = @intFromEnum(Op.LDA_I);
+    cartridge[0x0001] = 0x69;
+    cartridge[0x0002] = @intFromEnum(Op.TAX);
+    cartridge[0x0003] = @intFromEnum(Op.INX);
+    cartridge[0x0004] = @intFromEnum(Op.STA_ZPX);
+    cartridge[0x0005] = 0xF0;
+    cartridge[0x0006] = @intFromEnum(Op.RET);
     cartridge[0x7FFC] = 0x00;
     cartridge[0x7FFD] = 0x80;
-    var cpu: Cpu = Cpu;
+    var cpu: Cpu = undefined;
+    @memset(&cpu.mem, 0x00);
     cpu.insert(cartridge);
     cpu.exec();
-    for (0.., cpu.mem) |i, m| {
-        if (m) {
-            std.io.stderr.printf("0x%zx: 0x%x\n", i, m);
-        }
-    }
-    return cpu.mem[0x5A] == 0x69;
+    cpu.display();
+    try std.testing.expectEqual(0x69, cpu.mem[0x5A]);
 }
 
 pub fn main() void {
